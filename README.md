87661be7ca0e7757efe0a80e6f50d36e1322239f
alari <name.alari@gmail.com>
2012-08-14 13:35:26 +0400

    main config

--- README.md
+++ README.md
@@ -3,4 +3,25 @@ yii-showcase
 
 Showcase for tactic modules
 
-`git submodule add git@github.com:alari/yii-user.git protected/modules/user`
+How to
+============
+```
+git submodule add git://github.com/alari/yii-framework.git framework
+cd framework
+./yiic webapp ..
+cd ..
+git submodule add git://github.com/alari/yii-staticPages.git protected/modules/staticPages
+git submodule add git://github.com/alari/yii-imagesHolder.git protected/modules/imagesHolder
+git submodule add git://github.com/alari/yii-user.git protected/modules/user
+git submodule add git://github.com/alari/yii-i18n2ascii.git protected/extensions/i18n2ascii
+```
+
+Migrations
+============
+```
+cd protected
+./yiic migrate --migrationPath=imagesHolder.migrations
+./yiic migrate --migrationPath=staticPages.migrations
+./yiic migrate --migrationPath=user.migrations
+./yiic migrate
+```
--- protected/config/main.php
+++ protected/config/main.php
@@ -6,120 +6,134 @@
 // This is the main Web application configuration. Any writable
 // CWebApplication properties can be configured here.
 return array(
-	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Web Application',
+    'basePath' => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..',
+    'name' => 'Yii: Tactics Modules Showcase',
 
-	// preloading 'log' component
-	'preload'=>array('log'),
+    // preloading 'log' component
+    'preload' => array('log'),
 
-	// autoloading model and component classes
-	'import'=>array(
-		'application.models.*',
-		'application.components.*',
+    // autoloading model and component classes
+    'import' => array(
+        'application.models.*',
+        'application.components.*',
         // For user module
         'application.modules.user.models.*',
         'application.modules.user.components.*',
-	),
+    ),
 
-	'modules'=>array(
-		// uncomment the following to enable the Gii tool
-		/*
-		'gii'=>array(
-			'class'=>'system.gii.GiiModule',
-			'password'=>'Enter Your Password Here',
-			// If removed, Gii defaults to localhost only. Edit carefully to taste.
-			'ipFilters'=>array('127.0.0.1','::1'),
-		),
-		*/
+    'modules' => array(
+        // uncomment the following to enable the Gii tool
+        /*
+          'gii'=>array(
+              'class'=>'system.gii.GiiModule',
+              'password'=>'Enter Your Password Here',
+              // If removed, Gii defaults to localhost only. Edit carefully to taste.
+              'ipFilters'=>array('127.0.0.1','::1'),
+          ),
+          */
 
-        'user'=>array(
-            # encrypting method (php hash function)
+        'imagesHolder' => array(
+            'types' => array(
+                'materialSlides' => array(
+                    'maxNum' => 0,
+                    'preview' => 'tiny',
+                    'default' => 'big',
+                    'sizes' => array(
+                        "big" => "800x600",
+                        "tiny" => "120x120"
+                    )
+                )
+            )
+        ),
+
+        'user' => array(
             'hash' => 'md5',
-
-            # send activation email
             'sendActivationMail' => true,
-
-            # allow access for non-activated users
             'loginNotActiv' => false,
-
-            # activate user on registration (only sendActivationMail = false)
             'activeAfterRegister' => false,
-
-            # automatically login from registration
             'autoLogin' => true,
-
-            # registration path
             'registrationUrl' => array('/user/registration'),
-
-            # recovery password path
             'recoveryUrl' => array('/user/recovery'),
-
-            # login form path
             'loginUrl' => array('/user/login'),
-
-            # page after login
             'returnUrl' => array('/user/profile'),
-
-            # page after logout
             'returnLogoutUrl' => array('/user/login'),
         ),
-	),
 
-	// application components
-	'components'=>array(
-        'user'=>array(
+        'staticPages' => array(
+            "regions" => array(
+                "" => "-",
+            ),
+            //'view' => '//??? whatever view',
+            //'modelClass' => 'StaticPage'
+        ),
+    ),
+
+    // application components
+    'components' => array(
+        'user' => array(
             // enable cookie-based authentication
             'class' => 'WebUser',
-            'allowAutoLogin'=>true,
+            'allowAutoLogin' => true,
             'loginUrl' => array('/user/login'),
         ),
-		// uncomment the following to enable URLs in path-format
 
-		'urlManager'=>array(
-			'urlFormat'=>'path',
-            'showScriptName'=>false,
-			'rules'=>array(
-				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
-				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
-				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
-			),
-		),
+        'i18n2ascii' => array(
+            'class' => 'application.extensions.i18n2ascii.I18n2ascii'
+        ),
+
+        'urlManager' => array(
+            'urlFormat' => 'path',
+            'showScriptName' => false,
+            'rules' => array(
+                'page/<id:[\w\-]+>' => 'staticPages/staticPages/view',
+
+                '<controller:\w+>/<id:\d+>' => '<controller>/view',
+                '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+                '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
+            ),
+        ),
 
 
-		'db'=>array(
+        'db' => array(
             'tablePrefix' => 'tbl_',
-			'connectionString' => 'mysql:host=localhost;dbname=showcase',
-			'emulatePrepare' => true,
-			'username' => 'showcase',
-			'password' => 'showcase',
-			'charset' => 'utf8',
-		),
+            'connectionString' => 'mysql:host=localhost;dbname=showcase',
+            'emulatePrepare' => true,
+            'username' => 'showcase',
+            'password' => 'showcase',
+            'charset' => 'utf8',
+        ),
 
-		'errorHandler'=>array(
-			// use 'site/error' action to display errors
-			'errorAction'=>'site/error',
-		),
-		'log'=>array(
-			'class'=>'CLogRouter',
-			'routes'=>array(
-				array(
-					'class'=>'CFileLogRoute',
-					'levels'=>'error, warning',
-				),
-				// uncomment the following to show log messages on web pages
-				/*
-				array(
-					'class'=>'CWebLogRoute',
-				),
-				*/
-			),
-		),
-	),
+        'errorHandler' => array(
+            // use 'site/error' action to display errors
+            'errorAction' => 'site/error',
+        ),
+        'log' => array(
+            'class' => 'CLogRouter',
+            'routes' => array(
+                array(
+                    'class' => 'CFileLogRoute',
+                    'levels' => 'error, warning',
+                ),
+                // uncomment the following to show log messages on web pages
+                /*
+                    array(
+                        'class'=>'CWebLogRoute',
+                    ),
+                    */
+            ),
+        ),
 
-	// application-level parameters that can be accessed
-	// using Yii::app()->params['paramName']
-	'params'=>array(
-		// this is used in contact page
-		'adminEmail'=>'webmaster@example.com',
-	),
+        'image' => array(
+            'class' => 'imagesHolder.extensions.image.CImageComponent',
+            // GD or ImageMagick
+            'driver' => 'GD',
+        ),
+    ),
+
+    // application-level parameters that can be accessed
+    // using Yii::app()->params['paramName']
+    'params' => array(
+        // this is used in contact page
+        'adminEmail' => 'webmaster@example.com',
+    ),
 );
